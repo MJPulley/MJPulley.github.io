e72c57b618ba014759b48da59be254f8ce5a2663
Trinity Newsome <tnewsome2019@gmail.com>
2022-11-21 10:07:37 -0500

    .map function

---
+++ script.js
@@ -0,0 +1,20 @@
+function processSpeedCameras(list) {
+    console.log('fired speed cameras list');
+    const range = [...Array(15).keys()];
+    const newArray = range.map((item) => {
+      const index = getRandomIntInclusive(0, list.length);
+      return list[index];
+    });
+    return newArray;
+  }
+
+function initMap() {
+    console.log('initMap');
+    const map = L.map('map').setView([38.9897, -76.9378], 13);
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      maxZoom: 19,
+      attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    }).addTo(map);
+    return map;
+  }
+  
