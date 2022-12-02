1b9b4b53abba8cbc0f739cf875e713ecb8c4505a
MJPulley <mjpulley@outlook.com>
2022-12-02 18:13:35 -0500

    Edited home page/Placed markers on map

--- script.js
+++ script.js
@@ -1,42 +1,84 @@
-function injectHTML () {
+function getRandomIntInclusive(min, max) {
+  const newMin = Math.ceil(min);
+  const newMax = Math.floor(max);
+  return Math.floor(Math.random() * (newMax - newMin + 1) + newMin); // The maximum is inclusive and the minimum is inclusive
+}
 
+function processCameras(list) {
+  console.log('speed cameras list');
+  const range = [...Array(30).keys()];
+  const newArray = range.map((item) => {
+    const index = getRandomIntInclusive(0, list.length - 1);
+    return list[index];
+  });
+  return newArray;
 }
 
 function initMap() {
   const map = L.map('map').setView([38.7849, -76.8721], 13);
-    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      maxZoom: 19,
-      attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-    }).addTo(map);
-    return map;
-  }
-  
+  L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+  }).addTo(map);
+  return map;
+}
+
 function markerPlace(array, map) {
   map.eachLayer((layer) => {
     if (layer instanceof L.Marker) {
-        layer.remove();
+      layer.remove();
     }
   });
 
-array.forEach((item, index) => {
-  const {coordinates} = item.geocoded_column_1;
-    L.marker([coordinates[1], coordinates[0]]).addTo(map);
+  array.forEach((item, index) => {
+    console.log(item.location_1);
+
+    const northSouth = item.location_1.latitude;
+    const eastWest = item.location_1.longitude;
+
+    speedCameraMarker = L.marker([northSouth, eastWest]).addTo(map);
+
     if (index === 0) {
-      map.setView([coordinates[1], coordinates[0]], 10);
+      map.setView([northSouth, eastWest], 10);
     }
+
+    const address = item.street_address;
+    const postedSpeed = item.posted_speed;
   });
 }
 
-const page = initMap();
+async function getData() {
+  const url = 'https://data.princegeorgescountymd.gov/resource/mnkf-cu5c.json';
+  const data = await fetch(url);
+  const json = await data.json();
+  const reply = json.filter((item) => Boolean(item.location_1)).filter((item) => Boolean(item.school));
+  return reply;
+}
 
 async function mainEvent() {
-  if (arrayFromJson.data?.length > 0) {
-    form.addEventListener('input', (event) => {
-      //const filteredList = filterList(currentList, event.target.value);
-      //injectHTML(filteredList);
-      markerPlace();
+  const pageMap = initMap();
+
+  const form = document.querySelector('.main_form');
+  const submit = document.querySelector('#get-resto');
+  const loadAnimation = document.querySelector('.lds-ellipsis');
+  submit.style.display = 'none';
+
+  const mapData = await getData();
+
+  if (mapData?.length > 0) {
+    submit.style.display = 'block';
+
+    loadAnimation.classList.remove('lds-ellipsis');
+    loadAnimation.classList.add('lds-ellipsis_hidden');
+
+    let cameraList = [];
+
+    form.addEventListener('submit', async (submitEvent) => {
+      submitEvent.preventDefault();
+      cameraList = processCameras(mapData);
+      console.log(cameraList);
+      markerPlace(cameraList, pageMap);
     });
   }
 }
-
 document.addEventListener('DOMContentLoaded', async () => mainEvent());
